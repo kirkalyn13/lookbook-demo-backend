9e3b5f1794c4fe0c9960d02bf3746e791057ca12
Kirk Santos <kirkalyn13@gmail.com>
2024-02-24 16:11:48 +0800

    initial commit

---
+++ src/main/java/com/kirky/lookbookdemo/LookbookdemoApplication.java
@@ -0,0 +1,13 @@
+package com.kirky.lookbookdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookbookdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookbookdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kirky/lookbookdemo/LookbookdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirky.lookbookdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookbookdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
